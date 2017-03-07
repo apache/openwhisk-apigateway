5309ec9af86230435abbcd2ea6661bfcd3d6a94f
Carlos Santana <csantana@us.ibm.com>
2017-03-07 12:39:34 -0500

    try to skip docker builds for tests

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+set -x
 
 # Build script for Travis-CI.
 
@@ -10,7 +11,7 @@ WHISKDIR="$ROOTDIR/../openwhisk"
 # Install OpenWhisk
 cd $WHISKDIR/ansible
 
-ANSIBLE_CMD="ansible-playbook -i environments/local"
+ANSIBLE_CMD="ansible-playbook -i environments/local  -e docker_image_prefix=testing"
 
 $ANSIBLE_CMD setup.yml
 $ANSIBLE_CMD prereq.yml
@@ -20,7 +21,7 @@ $ANSIBLE_CMD apigateway.yml
 
 cd $WHISKDIR
 
-./gradlew distDocker -x :core:swift3Action:distDocker -x :core:pythonAction:distDocker -x :core:javaAction:distDocker -x :core:nodejsAction:distDocker
+./gradlew distDocker -x :core:swift3Action:distDocker -x :core:pythonAction:distDocker -x :core:javaAction:distDocker -x :core:nodejsAction:distDocker -x :core:actionProxy:distDocker -x :sdk:docker:distDocker -x :tests:dat:blackbox:badaction:distDocker -x :tests:dat:blackbox:badproxy:distDocker -PdockerImagePrefix=testing
 
 cd $WHISKDIR/ansible
 
@@ -32,5 +33,5 @@ export OPENWHISK_HOME=$WHISKDIR
 
 # Test
 cd $WHISKDIR
-./gradlew tests:test --tests apigw.healthtests.* --tests whisk.core.apigw.actions.test.* --tests whisk.core.cli.test.ApiGwTests
+./gradlew tests:test -x :core:swift3Action:distDocker -x :core:pythonAction:distDocker -x :core:javaAction:distDocker -x :core:nodejsAction:distDocker  -x :core:actionProxy:distDocker -x :sdk:docker:distDocker -x :tests:dat:blackbox:badaction:distDocker -x :tests:dat:blackbox:badproxy:distDocker --tests apigw.healthtests.* --tests whisk.core.apigw.actions.test.* --tests whisk.core.cli.test.ApiGwTests
 
