e37ff55afa6639a81b48071b3dc1a6ac150a0171
Matt Rutkowski <mrutkows@us.ibm.com>
2017-07-10 15:14:51 -0500

    Add Apache 2 license to all Lua source files.

--- scripts/lua/oauth/appid.lua
+++ scripts/lua/oauth/appid.lua
@@ -1,3 +1,20 @@
+--
+-- Licensed to the Apache Software Foundation (ASF) under one or more
+-- contributor license agreements.  See the NOTICE file distributed with
+-- this work for additional information regarding copyright ownership.
+-- The ASF licenses this file to You under the Apache License, Version 2.0
+-- (the "License"); you may not use this file except in compliance with
+-- the License.  You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
 local request = require 'lib/request'
 local cjson = require 'cjson'
 local utils = require 'lib/utils'
@@ -27,17 +44,17 @@ function _M.process(dataStore, token, securityObj)
   if err then
     request.err(500, 'error getting app id key: ' .. err)
   end
-  
+
   local key
   local keys = cjson.decode(res.body).keys
-  for _, v in ipairs(keys) do 
+  for _, v in ipairs(keys) do
     key = v
   end
   local result = cjose.validateJWS(token, cjson.encode(key))
   if not result then
     request.err(401, 'AppId key signature verification failed.')
     return nil
-  end 
+  end
   jwt_obj = cjson.decode(cjose.getJWSInfo(token))
   ngx.header['X-OIDC-Email'] = jwt_obj['email']
   ngx.header['X-OIDC-Sub'] = jwt_obj['sub']
