ac87866f2cc0135a15030a8f620b49a990be4e86
Matt Rutkowski <mrutkows@us.ibm.com>
2018-05-01 10:45:07 -0500

    Fix missing ASF License, EOL issues in .sh files.

--- build_config_supervisor.sh
+++ build_config_supervisor.sh
@@ -1,12 +1,30 @@
+#!/bin/sh
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 #ENV CONFIG_SUPERVISOR_VERSION 1.0.1-RC1
 export GOPATH=/usr/lib/go/bin
 export GOBIN=/usr/lib/go/bin
 export PATH=$PATH:/usr/lib/go/bin
 echo " ... installing api-gateway-config-supervisor  ... " \
-apk update 
-apk add gcc make git go 
-mkdir -p /tmp/api-gateway 
-curl -k -L https://github.com/adobe-apiplatform/api-gateway-config-supervisor/archive/${CONFIG_SUPERVISOR_VERSION}.tar.gz -o /tmp/api-gateway/api-gateway-config-supervisor-${CONFIG_SUPERVISOR_VERSION}.tar.gz 
+apk update
+apk add gcc make git go
+mkdir -p /tmp/api-gateway
+curl -k -L https://github.com/adobe-apiplatform/api-gateway-config-supervisor/archive/${CONFIG_SUPERVISOR_VERSION}.tar.gz -o /tmp/api-gateway/api-gateway-config-supervisor-${CONFIG_SUPERVISOR_VERSION}.tar.gz
 cd /tmp/api-gateway
 tar -xf /tmp/api-gateway/api-gateway-config-supervisor-${CONFIG_SUPERVISOR_VERSION}.tar.gz
 mkdir -p /tmp/go
@@ -20,17 +38,17 @@ ln -s /tmp/go /tmp/go-src/src/github.com/adobe-apiplatform/api-gateway-config-su
 GOPATH=/tmp/go/vendor:/tmp/go-src CGO_ENABLED=0 GOOS=linux /usr/lib/go/bin/godep  go build -ldflags "-s" -a -installsuffix cgo -o api-gateway-config-supervisor ./
 mv /tmp/go/api-gateway-config-supervisor /usr/local/sbin/
 
-echo "installing rclone sync ... " 
+echo "installing rclone sync ... "
 go get github.com/ncw/rclone
-mv /usr/lib/go/bin/rclone /usr/local/sbin/ 
+mv /usr/lib/go/bin/rclone /usr/local/sbin/
 mkdir -p /root/.config/rclone/
 cat <<EOF > /root/.config/rclone/rclone.conf
-[local] 
-type = local 
+[local]
+type = local
 nounc = true
 EOF
 
-echo " cleaning up ... " 
+echo " cleaning up ... "
 rm -rf /usr/lib/go/bin/src
 rm -rf /tmp/go
 rm -rf /tmp/go-src
