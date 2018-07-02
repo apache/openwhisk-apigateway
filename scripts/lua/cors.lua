be2107f5ab772690ad7a5f953f76b787919e0f95
Alex Song <songs@us.ibm.com>
2018-07-02 12:57:17 -0400

    Fix Access-Control-Allow-Headers header; Fix case when cors is not set

--- scripts/lua/cors.lua
+++ scripts/lua/cors.lua
@@ -25,21 +25,28 @@ function _M.processCall(resourceConfig)
   if resourceConfig.cors ~= nil then
     ngx.var.cors_origins = resourceConfig.cors.origin
     ngx.var.cors_methods = resourceConfig.cors.methods
+    -- preflight options call
     if resourceConfig.cors.origin ~= 'false' and ngx.req.get_method() == 'OPTIONS' then
+      -- 'Access-Control-Allow-Headers' response header is required for preflight requests that have 'Access-Control-Request-Headers' headers
+      local accessControlRequestHeaders = ngx.req.get_headers()['Access-Control-Request-Headers']
+      if accessControlRequestHeaders ~= nil then
+        ngx.header['Access-Control-Allow-Headers'] = accessControlRequestHeaders
+      end
       request.success(200)
     end
   end
 end
 
 function _M.replaceHeaders()
-  if ngx.var.cors_origins ~= nil then
+  if ngx.var.cors_origins ~= nil and ngx.var.cors_origins ~= '' then
     if ngx.var.cors_origins == 'false' then
       ngx.header['Access-Control-Allow-Origin'] = nil
       ngx.header['Access-Control-Allow-Methods'] = nil
+      ngx.header['Access-Control-Allow-Headers'] = nil
+      ngx.header['Access-Control-Allow-Credentials'] = nil
     else
       ngx.header['Access-Control-Allow-Origin'] = ngx.var.cors_origins == 'true' and (ngx.var.http_origin or '*') or ngx.var.cors_origins
       ngx.header['Access-Control-Allow-Methods'] = ngx.var.cors_methods or 'GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS'
-      ngx.header['Access-Control-Allow-Headers'] = ngx.req.get_headers()['Access-Control-Request-Headers']
       ngx.header['Access-Control-Allow-Credentials'] = 'true'
     end
   end
