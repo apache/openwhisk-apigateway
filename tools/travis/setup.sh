81a4f5ec76fab5ac8e3da9b334ba1e7ae71fabf6
David Grove <groved@us.ibm.com>
2019-11-22 13:34:17 -0500

    travis normalization dist, language, jdk, docker install

--- tools/travis/setup.sh
+++ tools/travis/setup.sh
@@ -19,18 +19,6 @@
 SCRIPTDIR=$(cd $(dirname "$0") && pwd)
 HOMEDIR="$SCRIPTDIR/../../.."
 
-sudo gpasswd -a travis docker
-sudo -E bash -c 'echo '\''DOCKER_OPTS="-H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock --storage-driver=overlay --userns-remap=default"'\'' > /etc/default/docker'
-
-# Docker
-sudo apt-get -y update -qq
-sudo apt-get -o Dpkg::Options::="--force-confold" --force-yes -y install docker-engine=1.12.0-0~trusty
-sudo service docker restart
-echo "Docker Version:"
-docker version
-echo "Docker Info:"
-docker info
-
 # jshint support
 sudo apt-get -y install nodejs npm
 sudo npm install -g jshint
